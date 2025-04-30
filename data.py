c7423d6f6be82ebce0631943a25288505db78faf
Anastasia-Veni <venividiamavi@yandex.ru>
2025-04-30 22:43:04 +0300

    data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+    "firstName": "Михал",
+    "lastName": "Соколов",
+    "address": "Можайское шоссе, 55",
+    "metroStation": 15,
+    "phone": "+7 999 888 54 54",
+    "rentTime": 4,
+    "deliveryDate": "2025-04-29",
+    "comment": "Привет!",
+    "color": ["BLACK"]
+}
