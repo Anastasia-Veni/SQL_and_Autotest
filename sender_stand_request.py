c1c4b42c7add638bc330991ad269d5879c1cc45e
Anastasia-Veni <venividiamavi@yandex.ru>
2025-04-30 22:43:39 +0300

    sender_stand_request.py

---
+++ sender_stand_request.py
@@ -0,0 +1,9 @@
+import configuration
+import requests
+
+def post_new_order(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, json=body)
+
+def get_order_by_track(track):
+    params = {"t": track}
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH, params=params)
